e025066b62a1bc4245590973438575188040c456
Dawid Jamróz <dawidjamroz4@gmail.com>
2022-03-22 16:43:23 +0100

    BladeView and parseFile, end Episode 14

--- Laravel_From_Scratch/resources/views/posts.blade.php
+++ Laravel_From_Scratch/resources/views/posts.blade.php
@@ -5,11 +5,19 @@
 
 <body>
 
-    <?php foreach ($posts as $post) : ?>
-        <article>
-            <?= $post; ?>
+    @foreach ($posts as $post)
+
+        <article class="{{ $loop->even ? 'foobar' : ''}}">
+            <h1> 
+               <a href="/posts/{{ $post->slug }}">  
+                    {{ $post->title }}
+              </a>                
+            </h1>
+        
+
+            <div>
+                {{ $post->excerpt }}
+            </div>
         </article>
-    <?php endforeach; ?>
-
-
+    @endforeach                                       
 </body>
