9ebbdd4df37fbe5c8a9c64f7b7cd5e33cf6e5e89
Dawid Jamróz <dawidjamroz4@gmail.com>
2022-03-26 18:54:13 +0100

    seeding, factories and view all posts by an author

--- Laravel_From_Scratch/database/migrations/2022_03_25_130023_create_posts_table.php
+++ Laravel_From_Scratch/database/migrations/2022_03_25_130023_create_posts_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePostsTable extends Migration
+//return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +16,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
             $table->foreignId('category_id');
             $table->string('slug')->unique();
             $table->string('title');
--- Laravel_From_Scratch/routes/web.php
+++ Laravel_From_Scratch/routes/web.php
@@ -2,12 +2,13 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::latest()->get()
     ]);
 });
 
@@ -21,5 +22,11 @@ Route::get('posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts
+    ]);
+});
+
+Route::get('authors:username', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 });
