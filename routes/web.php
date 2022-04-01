8d8bff30e0404dca3b7b25756d430080c0efb44f
Dawid Jamróz <dawidjamroz4@gmail.com>
2022-04-01 17:22:21 +0200

    Extract Dropdown Blade Component

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
         'posts' => Post::latest()->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
         
@@ -26,7 +26,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
         'currentCategory' => $category,
         'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
