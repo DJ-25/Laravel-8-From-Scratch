ed6cf6e989d28185a3a412bccf01f71903496e68
Dawid Jamróz <dawidjamroz4@gmail.com>
2022-04-02 00:08:12 +0200

    Search

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,16 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-    return view('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', function (Post $post) {
-        
-        return view('post', [
-            'post' => $post
-        ]);
-});
-
-Route::get('categories/{category:slug}', function (Category $category) {
+Route::get('categories/{category:slug}', function (Category $category) 
+{
     return view('posts', [
         'posts' => $category->posts,
         'currentCategory' => $category,
@@ -28,7 +18,8 @@ Route::get('categories/{category:slug}', function (Category $category) {
     ]);
 })->name('category');
 
-Route::get('authors/{author:username}', function (User $author) {
+Route::get('authors/{author:username}', function (User $author) 
+{
     return view('posts', [
         'posts' => $author->posts,
         'categories' => Category::all()
