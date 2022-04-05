717225d8dff7ef9342da0d624c32939018024b98
Dawid Jamróz <dawidjamroz4@gmail.com>
2022-04-05 15:52:28 +0200

    Build a register user page

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
 
 // Route::get('authors/{author:username}', function (User $author) 
 // {
