cd94c94a460676a67baa84efbf92ab039c5e6e21
Dawid Jamróz <dawidjamroz4@gmail.com>
2022-04-12 16:24:09 +0200

    Validate and Store Post Thumbnails

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -30,9 +30,10 @@ class PostController extends Controller
     }
 
     public function store() 
-    {
+    {   
         $attributes = request()->validate([
             'title' => 'required',
+            'thumbnail' => 'required|image',
             'slug' => ['required', Rule::unique('posts', 'slug')],
             'excerpt' => 'required',
             'body' => 'required',
@@ -40,6 +41,7 @@ class PostController extends Controller
         ]);
 
         $attributes['user_id'] = auth()->id();
+        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
 
         Post::create($attributes);
 
